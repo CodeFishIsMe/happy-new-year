624f8d33c7a80b6e7913d909ad4f087bdbb58a70
CodeFishIsMe <121720188+CodeFishIsMe@users.noreply.github.com>
2022-12-31 21:04:40 +0800

    Add files via upload

---
+++ 新年祝福.py
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg936\cocoartf2706
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 print(input("\uc0\u36755 \u20837 \u20320 \u30340 \u21517 \u23383 \u65306 "),"\u26032 \u24180 \u24555 \u20048 \u65281 \u36755 \u20837 6\u20197 \u32487 \u32493 ")\
+s=int(input())\
+if s==6:\
+    print("\uc0\u20320 \u25910 \u21040 \u20102 \u26469 \u33258 \u31243 \u24207 \u40060 \u30340 \u26032 \u24180 \u31069 \u31119 ")\
+    print("\uc0\u34429 \u28982 \u31243 \u24207 \u31616 \u38475 \u65292 \u20294 \u26159 \u24515 \u24847 \u26159 \u26377 \u30340 \u65292 \u24863 \u35874 \u20351 \u29992 \u26412 \u31243 \u24207 !")}
